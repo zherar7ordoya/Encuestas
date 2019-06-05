9d1e54dfea981102b4bca9a22c9feedb08cbaf3d
zherar7ordoya <zherar7ordoya@mail.ru>
2019-06-05 00:47:46 -0300

    Despliegue en Heroku

--- root/settings.py
+++ root/settings.py
@@ -118,4 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
